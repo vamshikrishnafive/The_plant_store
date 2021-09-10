4dbbb1c37252d7dec32e95da062bdc3a1aa90b3f
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-09-11 05:13:49 +0530

    fixed the order_id

--- Apis/controllers.js
+++ Apis/controllers.js
@@ -1,5 +1,6 @@
 import Razorpay from 'razorPay';
 import dotenv from 'dotenv';
+import { v4 as uuidv4 } from 'uuid';    
 import storeOrder from '../Models/order.js';
 dotenv.config()
 
@@ -12,22 +13,23 @@ export default class Payment {
     static welcomepage(req, res) { res.render('razorPay') }
     static createOrder(req, res) {
         var options = {
-            amount: 5000,
-            currency: "INR"
+            amount: 10000,
+            currency: "INR",
+            receipt: uuidv4(),
         };
         razorPay.orders.create(options, function (err, order) {
             if (!err) {
                 res.json(order);
-            }
+            } else console.log(err)
         });
     }
     static async fetchOrder(req, res) {
         const data = await razorPay.payments.fetch(req.body.razorpay_payment_id)
-        const { id, amount, currency, order_id, invoice_id, card_id, method, email, contact, status, acquirer_data, created_at } = data;
-        if (data.status === 'authorized') {
+        const { id, entity, amount, currency, order_id, card_id, method, email, contact, status, acquirer_data, created_at, captured, description, bank, wallet, fee } = data;
+        if (data.status === 'captured') {
             res.send('Payment Successful')
             try {
-                await storeOrder.create({ id, amount, currency, order_id, invoice_id, card_id, method, email, contact, status, acquirer_data, created_at })
+                await storeOrder.create({ id, entity, amount, currency, order_id, card_id, method, email, contact, status, acquirer_data, created_at, captured, description, bank, wallet, fee })
             } catch (error) {
                 console.error(error)
             }
